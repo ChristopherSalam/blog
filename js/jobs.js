30b37a456d62fbd60cad3a15dce4745f14175ad8
chrissalam <christopher.salam@gmail.com>
2015-02-02 16:25:03 -0800

    -under construction, w/ slidesjs

---
+++ js/jobs.js
@@ -0,0 +1,33 @@
+    $(function(){
+      $("#slides").slidesjs({
+        width: 940,
+        height: 528
+      });
+    });
+
+    $(function(){
+  $("#slides").slidesjs({
+    navigation: {
+      active: true,
+        // [boolean] Generates next and previous buttons.
+        // You can set to false and use your own buttons.
+        // User defined buttons must have the following:
+        // previous button: class="slidesjs-previous slidesjs-navigation"
+        // next button: class="slidesjs-next slidesjs-navigation"
+      effect: "slide"
+        // [string] Can be either "slide" or "fade".
+    }
+  });
+});
+
+$(function(){
+  $("#slides").slidesjs({
+    pagination: {
+      active: true,
+        // [boolean] Create pagination items.
+        // You cannot use your own pagination. Sorry.
+      effect: "slide"
+        // [string] Can be either "slide" or "fade".
+    }
+  });
+});
