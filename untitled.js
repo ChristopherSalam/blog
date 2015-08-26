332b5ecd93f2ff899114aaef266615a6455e0470
ChristopherSalam <christopher.salam@gmail.com>
2015-08-26 13:32:08 -0700

    Ready to push up /n complexity

--- untitled.js
+++ untitled.js
@@ -1,26 +1,42 @@
+// var commonCharacters = function (string1) {
+//   var string1Chars = {}, strNoDups = "", i, j;
 
-var commonCharacters = function (string1) {
+//   string1.split("").forEach(function (element) { string1Chars[element] = true; });
+//   for (i in string1Chars) { if (i) {strNoDups += i; } }
 
-  var string1Chars = {}, strNoDups = "", regExp, i, j;
+//   for (j = 1; j < arguments.length; j++) {
+//     strNoDups = strNoDups.match(new RegExp('[' + arguments[j] + ']', 'g')).join("");
+//   }
+//   return strNoDups;
+// };
 
-  string1.split("").forEach(function (element) { string1Chars[element] = true; });
-  for (i in string1Chars) { if (i) {strNoDups += i; } }
-
-  for (j = 1; j < arguments.length; j++) {
-    regExp = new RegExp('[' + arguments[j] + ']', 'g');
-    strNoDups = strNoDups.match(regExp).join("");
-  }
-  return strNoDups;
-};
-
-
-
-
-console.log(commonCharacters('aceexivoue', 'aegihobue'));
-                  // --> 'aeiou'
-console.log(commonCharacters('aecexivou', 'aegihobu', 'aeefilou'));
-                  // --> 'aeiou'
-console.log(commonCharacters('dcba', 'abcd'));
+// console.log(commonCharacters('aceexivoue', 'aegihobue'));
+//                   // --> 'aeiou'
+// console.log(commonCharacters('aecexivou', 'aegihobu', 'aeefilou'));
+//                   // --> 'aeiou'
+// console.log(commonCharacters('dcba', 'abcd'));
                   // --> 'dcba'
+
+var longestRun = function (string) {
+  var max = 0, longRun;
+  string.replace(/(\w+)\1+/g, function (set) {
+    if (set.length > max) {
+      max = set.length;
+      longRun = set;
+    } return set;
+  });
+  return (max === 0) ? [0, 0] :
+  [ string.match('[' + longRun + ']').index,
+    string.match('[' + longRun + ']').index + max - 1 ];
+}
+
+//Test Suite
+
+console.log(longestRun("abbbc"));
+                  // --> 'aeiou'
+console.log(longestRun("abc"));
+                  // --> 'aeiou'
+console.log(longestRun("abbcc"));
+                  // --> 'aeiou'
 
                     // return set.replace(/(\w+)\1+/gi,function(set){return set[0]});
